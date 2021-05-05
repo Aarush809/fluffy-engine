e6355781e6a6b337b13eb7231b906bcb480f40d7
Aarush809 <71543434+Aarush809@users.noreply.github.com>
2021-05-05 18:53:51 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,23 @@
+function addroom(){
+
+function addroom(){
+ room_name=document.getElementById("addroom").value;
+ firebase.database().ref("/").child(room_name).update({
+     purpose:"adding room name"
+ }); 
+ localStorage.setItem("room_name", room_name);
+ window.location="kwitter_page.html";
+ 
+
+
+}
+}
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey =
+childSnapshot.key;
+ Room_names = childKey;
+ //Start code
+ //End code
+ });});}
+getData();
